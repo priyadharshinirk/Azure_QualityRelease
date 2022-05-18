ae082ea1915e3c01b514a8dc0d904296e6b453fe
Priyadharshini R <Priyadharshini.R@shell.com>
2022-05-18 11:06:16 +0530

    modified access key

--- terraform/environment/test/main.tf
+++ terraform/environment/test/main.tf
@@ -10,7 +10,7 @@ terraform {
     storage_account_name = "${var.storage_account_name}"
     container_name       = "${var.container_name}"
     key                  = "${var.key}"
-    access_key           = "${var.access_key}"
+    access_key           = "kp6lp19+SnhkF3X8oCyDSusuAkX2TBjpsDAbtBh/Lost0ccNHnBpLxFTT2hWEbzbiTCEKh3PXuKdkKoKhXr+uw=="
   }
 }
 module "network" {
